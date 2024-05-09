01cc403f90340117bb96b84d650b3884146ecbab
Axumawityemaneh <yemanehaxumawit@gmail.com>
2024-05-09 22:56:21 +0300

    add files

--- follower.py
+++ follower.py
@@ -9,13 +9,30 @@ class UserSystem:
         self.user_id =1
 
     def add_user(self,username):
-        user = user(username)
-        user.user_id= self.user_id
-        self.user_id += 1
+        user = User(username)
+
+        user.user_id= self.user_id_counter
+        self.user_id_counter += 1
         self.users.append(user)
-UserSystem = UserSystem()
-UserSystem.add_user("Naserian")
-UserSystem.add_user("Samuria")
+    
+User_System = UserSystem()
+User_System.add_user("Naserian")
+User_System.add_user("Samuria")
 
 print("username")
+user_followers =[
+    {"useename": "user1","followers":["followers","followers2","followers3"]},
+    {"username": "user2","followers":["follower4","followers5"]},
+    print("username")
+]
+def get_followers(username):
+    for user in user_followers:
+        if user["username"]==username:
+            return user["followers"]
+        else:
+            return[]
+        print(get_followers(user1))
+        print(get_followers("user3"))
+
+    
         
